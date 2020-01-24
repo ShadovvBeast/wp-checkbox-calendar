29022e730ff2f5e497d62128d8ba66636038cf47
ShadowBeast <ShadovvBeast@gmail.com>
2020-01-24 13:24:25 +0200

    fullcalendar interactions and css

--- js/calendar.js
+++ js/calendar.js
@@ -1,19 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('init fullcalendar');
     const calendarEl = document.getElementById('calendar');
-    const calendar = new _fullcalendar_core.Calendar(calendarEl, {
+    const calendar = new FullCalendar.Calendar(calendarEl, {
         locales: FullCalendarLocales,
         locale: 'he',
-        plugins: [_fullcalendar_daygrid.default, _fullcalendar_interaction.default],
+        plugins: [FullCalendarDayGrid.default, FullCalendarInteraction.default],
         defaultView: 'dayGridMonth',
         height: 'auto',
         fixedWeekCount: false,
         dateClick: function(info) {
-            alert('Clicked on: ' + info.dateStr);
-            alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-            alert('Current view: ' + info.view.type);
-            // change the day's background color just for fun
-            info.dayEl.style.backgroundColor = 'red';
+            const today = new Date();
+            today.setHours(0,0,0,0);
+            if (info.date >= today)
+                alert('START!');
+            // alert('Clicked on: ' + info.dateStr);
+            // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+            // alert('Current view: ' + info.view.type);
+            // // change the day's background color just for fun
+            // info.dayEl.style.backgroundColor = 'red';
         }
     });
 
--- wp-checkbox-calendar.php
+++ wp-checkbox-calendar.php
@@ -15,14 +15,15 @@ function wp_checkbox_calendar_shortcode($atts) {
 }
 
 function wp_checkbox_calendar_enqueue() {
-    wp_enqueue_script('fullcalendar-core', 'https://bundle.run/@fullcalendar/core@4.3.1');
+    wp_enqueue_script('fullcalendar-core', 'https://unpkg.com/@fullcalendar/core/main.min.js');
     wp_enqueue_style('fullcalendar-core', 'https://unpkg.com/@fullcalendar/core@4.3.1/main.min.css');
     wp_enqueue_script('fullcalendar-core-locales-he', 'https://cdn.jsdelivr.net/npm/@fullcalendar/core@4.3.1/locales/he.js');
-    wp_enqueue_script('fullcalendar-daygrid', 'https://bundle.run/@fullcalendar/daygrid@4.3.0');
+    wp_enqueue_script('fullcalendar-daygrid', 'https://unpkg.com/@fullcalendar/daygrid/main.min.js');
     //wp_enqueue_style('fullcalendar-daygrid', 'https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@4.3.0/main.min.css');
-    wp_enqueue_script('fullcalendar-interactions', 'https://bundle.run/@fullcalendar/interaction@4.3.0');
+    wp_enqueue_script('fullcalendar-interactions', 'https://unpkg.com/@fullcalendar/interaction/main.min.js');
     wp_enqueue_script('wp-checkbox-calendar-calendar', plugins_url('/js/calendar.js', __FILE__));
     wp_enqueue_style('wp-checkbox-calendar-style', plugins_url('wp-checkbox-calendar.css', __FILE__));
 }
+register_activation_hook( __FILE__, 'jal_install' );
 add_action('wp_enqueue_scripts', 'wp_checkbox_calendar_enqueue');
 add_shortcode('wp-checkbox-calendar', 'wp_checkbox_calendar_shortcode');
