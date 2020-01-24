d058a44ad2f5f318dd01495f05f49316263553d6
ShadowBeast <ShadovvBeast@gmail.com>
2020-01-24 14:44:25 +0200

    db (manual) and ajax

--- js/calendar.js
+++ js/calendar.js
@@ -1,3 +1,42 @@
+function JustADate(initDate){
+    var utcMidnightDateObj = null
+    // if no date supplied, use Now.
+    if(!initDate)
+        initDate = new Date();
+
+    // if initDate specifies a timezone offset, or is already UTC, just keep the date part, reflecting the date _in that timezone_
+    if(typeof initDate === "string" && initDate.match(/((\+|-)\d{2}:\d{2}|Z)$/gm)){
+        utcMidnightDateObj = new Date( initDate.substring(0,10) + 'T00:00:00Z');
+    } else {
+        // if init date is not already a date object, feed it to the date constructor.
+        if(!(initDate instanceof Date))
+            initDate = new Date(initDate);
+        // Vital Step! Strip time part. Create UTC midnight dateObj according to local timezone.
+        utcMidnightDateObj = new Date(Date.UTC(initDate.getFullYear(),initDate.getMonth(), initDate.getDate()));
+    }
+
+    return {
+        toIsoString:()=>utcMidnightDateObj.toIsoString(),
+        getUTCDate:()=>utcMidnightDateObj.getUTCDate(),
+        getUTCDay:()=>utcMidnightDateObj.getUTCDay(),
+        getUTCFullYear:()=>utcMidnightDateObj.getUTCFullYear(),
+        getUTCMonth:()=>utcMidnightDateObj.getUTCMonth(),
+        setUTCDate:(arg)=>utcMidnightDateObj.setUTCDate(arg),
+        setUTCFullYear:(arg)=>utcMidnightDateObj.setUTCFullYear(arg),
+        setUTCMonth:(arg)=>utcMidnightDateObj.setUTCMonth(arg),
+        addDays:(days)=>{
+            utcMidnightDateObj.setUTCDate(utcMidnightDateObj.getUTCDate + days)
+        },
+        toString:()=>utcMidnightDateObj.toString(),
+        toLocaleDateString:(locale,options)=>{
+            options = options || {};
+            options.timezone = "UTC";
+            locale = locale || "en-EN";
+            return utcMidnightDateObj.toLocaleDateString(locale,options)
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const calendarEl = document.getElementById('calendar');
     const calendar = new FullCalendar.Calendar(calendarEl, {
@@ -7,11 +46,21 @@ document.addEventListener('DOMContentLoaded', function() {
         defaultView: 'dayGridMonth',
         height: 'auto',
         fixedWeekCount: false,
-        dateClick: function(info) {
+        dateClick: async function(info) {
             const today = new Date();
             today.setHours(0,0,0,0);
-            if (info.date >= today)
-                alert('START!');
+            if (info.date >= today) {
+                today.setDate(today.getDate() + 1);
+                console.log(await (await fetch(ajax_object.ajax_url, {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Cache-Control': 'no-cache',
+                    },
+                    body: "action=checkbox_check&date=" + today.toISOString().split('T')[0]
+                })).text());
+            }
             // alert('Clicked on: ' + info.dateStr);
             // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
             // alert('Current view: ' + info.view.type);
--- wp-checkbox-calendar.php
+++ wp-checkbox-calendar.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: WP Checkbox Calendar
  * Plugin URI: https://github.com/ShadovvBeast/wp-checkbox-calendar
@@ -9,11 +8,15 @@
  * Author: ShadowBeast (Asaf Levy)
  * Author URI: https://github.com/ShadovvBeast
  */
-
 function wp_checkbox_calendar_shortcode($atts) {
     return '<div id="calendar"></div>';
 }
 
+function wp_checkbox_check() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'cc_checks';
+    $wpdb->insert($table_name, ['user_id'=> get_current_user_id(), 'check_date' => $_POST['date']]);
+}
 function wp_checkbox_calendar_enqueue() {
     wp_enqueue_script('fullcalendar-core', 'https://unpkg.com/@fullcalendar/core/main.min.js');
     wp_enqueue_style('fullcalendar-core', 'https://unpkg.com/@fullcalendar/core@4.3.1/main.min.css');
@@ -22,8 +25,11 @@ function wp_checkbox_calendar_enqueue() {
     //wp_enqueue_style('fullcalendar-daygrid', 'https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@4.3.0/main.min.css');
     wp_enqueue_script('fullcalendar-interactions', 'https://unpkg.com/@fullcalendar/interaction/main.min.js');
     wp_enqueue_script('wp-checkbox-calendar-calendar', plugins_url('/js/calendar.js', __FILE__));
+    wp_localize_script( 'wp-checkbox-calendar-calendar', 'ajax_object',
+                array( 'ajax_url' => admin_url( 'admin-ajax.php' ), 'we_value' => 1234 ) );
     wp_enqueue_style('wp-checkbox-calendar-style', plugins_url('wp-checkbox-calendar.css', __FILE__));
 }
-register_activation_hook( __FILE__, 'jal_install' );
+
 add_action('wp_enqueue_scripts', 'wp_checkbox_calendar_enqueue');
+add_action( 'wp_ajax_checkbox_check', 'wp_checkbox_check');
 add_shortcode('wp-checkbox-calendar', 'wp_checkbox_calendar_shortcode');
